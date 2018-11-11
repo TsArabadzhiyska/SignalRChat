af1fe5799a4855b4dcf1d7ca5ae6ca51b448cd97
TsArabadzhiyska <c.arabadzhiyska@abv.bg>
2018-11-11 18:17:54 +0200

    Initial commit

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        /*
+        The SendMessage method can be called by any connected client.
+            It sends the received message to all clients.SignalR code 
+            is asynchronous to provide maximum scalability.
+        */
+    }
+}
